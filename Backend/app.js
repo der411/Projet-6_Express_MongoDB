de12f2181e27b4a1228cd8edab5c2196dd878f9a
der411 <vaitilingomvincent@gmail.com>
2024-06-19 19:37:09 +0200

    Créations middleware Get et Post + implémentation CORS

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,25 @@
+const e = require('express');
 const express = require('express');
+
 const app = express();
 
-app.use('/api/books', (req, res, next) => {;
+app.use(express.json());
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+ });
+
+ app.post('/api/books', (req, res, next) => {
+   console.log(req.body);
+   res.status(201).json({
+     message: 'Objet créé !'
+   });
+ });
+
+app.get('/api/books', (req, res, next) => {;
 const books = [
    {
      id: '1',
